f32c8b70912124de1ae6f3bae1e973dbd6a4464b
ParkHyunSeok <luckseok1@gmail.com>
2022-10-19 16:55:01 +0900

    [coplit] Section 2 Daily coding 🌟 10번문제

--- coplit/[S2]DailyCoding/10_insertDash/index.js
+++ coplit/[S2]DailyCoding/10_insertDash/index.js
@@ -1,16 +1,3 @@
-/**
-문제
-문자열을 입력받아 연속된 한자리 홀수 숫자 사이에 '-'를 추가한 문자열을 리턴해야 합니다.
-
-입력
-인자 1 : str
-string 타입의 숫자 문자열
-
-출력
-string 타입을 리턴해야 합니다.
-주의 사항
-0은 짝수로 간주합니다.
- */
 function insertDash(str) {
   let result = str[0];
   for (let i = 1; i < str.length; i++) {
