84aa6f13b36c339b6596ecab601bde044ff7d84f
Hyun Seok Park <luckseok1@gmail.com>
2022-11-09 23:11:45 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,18 @@
+function solution(age) {
+    let alpha = {
+        0 : "a",
+        1 : "b",
+        2 : "c",
+        3 : "d",
+        4 : "e",
+        5 : "f",
+        6 : "g",
+        7 : "h",
+        8 : "i",
+        9 : "j"
+    }
+    
+    let newAge = age + "";
+    
+    return [...newAge].map((el) => alpha[el]).join("");
+}
