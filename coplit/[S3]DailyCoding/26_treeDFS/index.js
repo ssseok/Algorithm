9f5d0f40439996623619c6305b9d7a8361048cd3
Hyun Seok Park <luckseok1@gmail.com>
2022-11-22 09:07:30 +0900

    [coplit] Section 3 Daily coding 🌟 26번문제

---
+++ coplit/[S3]DailyCoding/26_treeDFS/index.js
@@ -0,0 +1,37 @@
+let dfs = function (node) {
+  // TODO: 여기에 코드를 작성합니다.
+  let values = [node.value];
+
+  node.children.forEach((n) => {
+    values = values.concat(dfs(n));
+  });
+
+  return values;
+};
+
+// 이 아래 코드는 변경하지 않아도 됩니다. 자유롭게 참고하세요.
+let Node = function (value) {
+  this.value = value;
+  this.children = [];
+};
+
+// 위 Node 객체로 구성되는 트리는 매우 단순한 형태의 트리입니다.
+// membership check(중복 확인)를 따로 하지 않습니다.
+Node.prototype.addChild = function (child) {
+  this.children.push(child);
+  return child;
+};
+
+// 입출력 예시
+let root = new Node(1);
+let rootChild1 = root.addChild(new Node(2));
+let rootChild2 = root.addChild(new Node(3));
+let leaf1 = rootChild1.addChild(new Node(4));
+let leaf2 = rootChild1.addChild(new Node(5));
+let output = dfs(root);
+console.log(output); // --> [1, 2, 4, 5, 3]
+
+leaf1.addChild(new Node(6));
+rootChild2.addChild(new Node(7));
+output = dfs(root);
+console.log(output); // --> [1, 2, 4, 6, 5, 3, 7]
