e1362419ab2dbbea9cb2b49955c4518c7484d2fb
Hyun Seok Park <luckseok1@gmail.com>
2023-02-24 18:06:55 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,12 @@
+function solution(my_str, n) {
+      let strArr = my_str.split("");
+
+    
+    let ans = [];
+    
+    while(strArr.length > 0){
+        ans.push(strArr.splice(0,n).join(""));
+    }
+    
+    return ans;
+}
