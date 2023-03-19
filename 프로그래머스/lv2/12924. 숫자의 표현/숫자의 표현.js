11cd93c695544749d8904509a2b3d8c1312fba59
Hyun Seok Park <luckseok1@gmail.com>
2023-03-19 18:34:03 +0900

    [level 2] Title: 숫자의 표현, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    // let arr = [];
+    // for(let i=1; i<=n; i++) {
+    //     if(n % i === 0) {
+    //         arr.push(i)
+    //     }
+    // }
+    // return arr.length
+    let answer = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0 && i % 2 === 1) answer++;
+    }
+    return answer;
+
+    
+}
