cc26d869bd13f3f07f356b4ef7156eb146869134
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-04-13 16:00:10 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,14 +1,13 @@
 function solution(s){
-    var answer = true;
-    let countp =0;
-    let county =0;
-    for(let i=0; i<s.length; i++){
-        if(s[i]=="p"||s[i]=="P"){
-            countp++;
-        }else if(s[i]=="y"||s[i]=="Y"){
-            county++;
+    let arr =[]
+    let arr2 =[]
+    for(let i=0; i<s.length; i++) {
+        if(s[i] === "p" || s[i] === "P"){
+            arr.push(i)
+        }
+        if(s[i] === "y" || s[i] === "Y") {
+            arr2.push(i)
         }
     }
-
-    return countp==county? true:false
+    return arr.length === arr2.length
 }
