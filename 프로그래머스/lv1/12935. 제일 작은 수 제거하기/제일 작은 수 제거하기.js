ebffb233bf6ea17967ca3487cf29760e3644c519
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-04-14 20:14:24 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.00 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,28 +1,4 @@
 function solution(arr) {
-
-  let answer = []
-  let min = arr[0]
-  // 최솟값의 기준으로 배열의 첫번째 요소를 넣는다.
-
-  if(arr.length == 1){
-    return [-1];
-    // 배열의 요소가 하나 밖에 없는 경우
-    // 비교할 대상이 없기 때문에 -1을 배열에 담아서 return
-  }else{ // 배열의 요소가 2개 이상일 경우
-    for(let i = 0; i<arr.length; i++){
-      if(min > arr[i]){
-      // if문을 사용해서 min의 값과 arr[i]를 비교해서
-      // 더 작은 값이 나오면
-        min = arr[i]
-        // min의 값을 바꾼다.
-      }
-    }
-  }
-
-  arr.splice(arr.indexOf(min),1)
-  // arr 배열에서 가장 작은 숫자의 index 요소를 넣고 삭제한다.
-    answer = arr;
-  // arr 배열 원본이 바뀌었기 때문에 answer에 넣는다.
-
-  return answer;
+    if(arr.length === 1 || arr.length === 0) return [-1]
+    return arr.filter((a) => a !== Math.min(...arr))
 }
