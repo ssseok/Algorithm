f5e31ee25f8aa6f08b47a6b11a98c975b9b1fc05
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-04-15 19:02:36 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,4 @@
 function solution(n) {
-    var answer = '';
-    for(let i=0; i<n; i++){
-        if(i%2==0){
-            answer += "수"
-        }else{
-            answer += "박"
-        }
-    }
-    return answer;
+ return "수박".repeat(n).substring(0,n)
+    
 }
