34b16162b86bf2925f44f5cc81ea70bef6279b55
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-04-16 23:33:39 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,7 +1,3 @@
 function solution(s) {
-    let filter = [...s].filter(a => a === a.toLowerCase())
-    let filter2 = [...s].filter(a => a === a.toUpperCase())
-    let sort = filter.sort()
-    let sort2 = filter2.sort()
-    return [...sort.reverse(), ...sort2.reverse()].join("")
+ return s.split('').sort().reverse().join('');
 }
