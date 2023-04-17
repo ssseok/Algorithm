762563bfc0075ee0471d59fa90376f7911dbf5f4
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-04-17 18:13:36 +0900

    [level 2] Title: 올바른 괄호, Time: 5.32 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,8 @@
 function solution(s){
     let count = 0;
     for(const a of s) {
-        if(a === "(") {
-            count++;
-        } else {
-            count--;
-        }
-    if(count < 0) return false;
+        a === "(" ? count++ : count--;
+        if(count < 0) return false
     }
-    return count === 0;
+    return count === 0
 }
