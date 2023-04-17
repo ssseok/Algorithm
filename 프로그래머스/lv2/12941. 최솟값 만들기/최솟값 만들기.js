efe22c0bf4722c13b06a0d8de650417c1afbc737
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-04-17 18:28:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.82 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,13 +1,10 @@
 function solution(A,B){
-    // 값 담는 배열
-    let C = []
-    // 최솟값을 구해야해서 B의 배열을 큰수부터 나열
-    let Bsort = B.sort((a,b) => b-a)
-    let Asort = A.sort((a,b) => a-b)
-    // for문으로 배열의 길이로 돌아 C에 담는다.
+    let answer = 0
+    let sort1 = A.sort((a,b) => a-b);
+    let sort2 = B.sort((a,b) => b-a);
     for(let i=0; i<A.length; i++) {
-         C.push(Asort[i] * Bsort[i])
+        answer += Math.min(sort1[i]) * Math.min(sort2[i])
+        
     }
-    // reduce를 사용하여 누적값을 담아 더한다.
-    return C.reduce((acc,cur) => acc + cur, 0)
+    return answer
 }
