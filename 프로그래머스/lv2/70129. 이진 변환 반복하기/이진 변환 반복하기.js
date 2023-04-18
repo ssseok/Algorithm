cc771c1ffa1b3c15d19fcf19c0dfdffd8c0e9f03
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-04-18 14:52:47 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.70 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,13 +1,14 @@
 function solution(s) {
-    let zero =0
+    let deleteZero =0
     let count =0
     while(s.length!==1){
         const originLen = s.length
-        s=[...s].filter(v=>v==='1').join('')
+        s=s.split('').filter(v=>v==='1').join('')
         const len = s.length
-        zero+=originLen-len
+        deleteZero+=originLen-len
         s=len.toString(2)
         count++
     }
-    return[count,zero]
+    return[count,deleteZero]
+    
 }
