d78092ea8ef7a302180235bb93a3b4098e67f5e6
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-05-29 20:10:15 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n)
+{
+ let a = 0;
+    while(n > 0) {
+        if(n % 2 === 0) {
+            n /= 2;
+        } else {
+            n--;
+            a++;
+        }
+    }
+    
+    return a;
+}
