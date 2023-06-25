cb4e870948fd6197b48518c2d877ed19dccd310c
박현석 Park Hyun Seok <luckseok1@gmail.com>
2023-06-25 19:36:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.96 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,10 +1,18 @@
-function solution(s){
-  const stack = [];
-
-      for(let i = 0 ; i < s.length ; i++ ){
-        if( !stack.length || stack[stack.length-1] !== s[i] ) stack.push(s[i]);
-          else stack.pop();
+function solution(s)
+{
+  let stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        if (stack.length !== 0 && stack[stack.length - 1] === s[i]) {
+            // If the top of the stack is the same as the current character, pop from the stack
+            stack.pop();
+        } else {
+            // If the stack is empty or the top of the stack is not the same as the current character, push onto the stack
+            stack.push(s[i]);
+        }
     }
 
-  return stack.length ? 0 : 1;
+    // If the stack is empty, return 1, else return 0
+    return stack.length === 0 ? 1 : 0;
+    
 }
